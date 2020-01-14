6090a758d87b3000eba230a10b4057f8936a5096
joseph adjare <joseph.adjare@newlook.com>
2020-01-14 15:09:00 +0000

    Add AD Group authorisation requirement and handler

---
+++ 5-WebApp-AuthZ/5-2-Groups/AuthorizationHandlers/HasGroupAuthorizationHandler.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApp_OpenIDConnect_DotNet.Services.MicrosoftGraph;
+
+namespace WebApp_OpenIDConnect_DotNet.AuthorizationHandlers
+{
+    public class HasGroupAuthorizationHandler : AuthorizationHandler<IsGroupAuthorized>
+    {
+        private readonly ITokenAcquisition _tokenAcquisition;
+        private readonly IMSGraphService _graphService;
+
+        public HasGroupAuthorizationHandler(ITokenAcquisition tokenAcquisition, IMSGraphService MSGraphService)
+        {
+            _tokenAcquisition = tokenAcquisition;
+            _graphService = MSGraphService;
+        }
+
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context,
+            IsGroupAuthorized requirement)
+        {
+            if (requirement.IsMemberOf(await GetUserGroups()))
+            {
+                context.Succeed(requirement);
+            }
+            else
+            {
+                context.Fail();
+            }
+        }
+
+        private async Task<IList<Group>> GetUserGroups()
+        {
+            string accessToken =
+                await _tokenAcquisition.GetAccessTokenOnBehalfOfUserAsync(new[] {"User.Read", "Directory.Read.All"});
+
+            await _graphService.GetMeAsync(accessToken);
+
+            IList<Group> groups = await _graphService.GetMyMemberOfGroupsAsync(accessToken);
+
+            return groups;
+        }
+    }
+}
---
+++ 5-WebApp-AuthZ/5-2-Groups/AuthorizationHandlers/IsGroupAuthorized.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApp_OpenIDConnect_DotNet.AuthorizationHandlers
+{
+    public class IsGroupAuthorized : IAuthorizationRequirement
+    {
+        public IsGroupAuthorized(string groupId) : this(new[] {groupId}){}
+
+        public IsGroupAuthorized(params string[] groupIds) : this(groupIds as IEnumerable<string>){}
+
+        public IsGroupAuthorized(IEnumerable<string> groupIds)
+        {
+            AuthorizedGroups = groupIds ?? throw new ArgumentNullException(nameof(groupIds));
+        }
+
+        public IEnumerable<string> AuthorizedGroups { get; }
+
+        public bool IsMemberOf(IEnumerable<Group> groups)
+        {
+            return groups.Any(group => this.AuthorizedGroups.Contains(group.Id));
+        }
+    }
+}
